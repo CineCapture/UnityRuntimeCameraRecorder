4a22918d018f87b43482247a3c72810488f7fd15
End3rbyte <end3rbyte@outlook.com>
2026-09-25 12:03:55 +0200

    Use automatic assembly versions

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2026 End3rbyte")]
 [assembly: ComVisible(false)]
 [assembly: Guid("8746502F-1F35-4EFA-AEA9-F4B494013F9E")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0")]
